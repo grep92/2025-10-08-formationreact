2c7bbc12b30a85918dab9462e362711079bde5e8
Grep92 <grep@netc.eu>
2025-10-08 14:12:24 +0100

    Renommage des extensions de fichier.

---
+++ src/components/ui/Button/Button.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Button.css";
+
+const Button = (props) => {
+  return (
+    <button className="Button" data-testid="Button">
+      Button Component
+    </button>
+  );
+};
+
+export default Button;
